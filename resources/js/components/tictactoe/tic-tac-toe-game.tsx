bc4ced4ac9babeb8c527904d2b1da010e3c5890d
Ryan <ryandam1473@googlemail.com>
2026-02-08 23:10:58 +0000

    Add SCSS to tic tac toe game

---
+++ resources/js/components/tictactoe/tic-tac-toe-game.tsx
@@ -0,0 +1,8 @@
+export default function TicTacToeGame()
+{
+    return (
+        <div className="tic-tac-toe board">
+            <button className="square">X</button>
+        </div>
+    );
+}
--- resources/js/pages/ticTacToe.tsx
+++ resources/js/pages/ticTacToe.tsx
@@ -2,6 +2,8 @@ import AppLayout from '@/layouts/app-layout';
 import { Head } from '@inertiajs/react';
 import { BreadcrumbItem } from '@/types';
 import { tictactoe } from '@/routes';
+import TicTacToeGame from '@/components/tictactoe/tic-tac-toe-game';
+import "../../css/tictactoe.scss"
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -10,12 +12,13 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ]
 
-export default function TicTacToe({})
+export default function TicTacToe()
 {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Tic Tac Toe" />
-            <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+            <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4 bg-neutral-800">
+                <TicTacToeGame/>
             </div>
         </AppLayout>
     );
