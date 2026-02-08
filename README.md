783cbd1d812a93532b055a6c98dd3971351071ab
Ryan <ryandam1473@googlemail.com>
2026-02-08 21:07:25 +0000

    Add type annotation, add basic loop to render name using a function

--- README.md
+++ README.md
@@ -12,3 +12,10 @@
 1. Run `docker compose up --build` to build the image and start the container
 2. Run `docker exec -it php_app bun run dev` to build and run the frontend
 3. Visit [Localhost](http://localhost)
+
+## Adminer
+Visit http://localhost:8080
+- Server: postgres
+- User: root
+- Password: password
+- Database: laravel
--- resources/js/pages/users.tsx
+++ resources/js/pages/users.tsx
@@ -1,6 +1,6 @@
 import AppLayout from '@/layouts/app-layout';
 import { Head } from '@inertiajs/react';
-import { BreadcrumbItem } from '@/types';
+import { BreadcrumbItem, User } from '@/types';
 import { users } from '@/routes';
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -10,12 +10,20 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ]
 
-export default function Users()
+function renderUsernames(users: User[])
+{
+    return users.map(user =>
+        <p>{user.name}</p>
+    );
+}
+
+export default function Users({ users }: { users: User[] })
 {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Users" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+                {renderUsernames(users)}
             </div>
         </AppLayout>
     );
